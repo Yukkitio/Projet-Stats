cf4ce5d0db5e8630bacacf9df2111cdc0873909d
Victor <victor20011204@gmail.com>
2023-06-30 21:05:35 +0200

    Ajout Game Cards
    + Ajout basic  design des game card
    + Responsive par default

--- src/pages/game_stats_ui.js
+++ src/pages/game_stats_ui.js
@@ -1,11 +1,46 @@
 import React from "react";
 
-function GameStats() {
+export default function GameStats() {
+  const gameCardsData = [
+      {
+          image: 'https://picsum.photos/500/300/?image=10',
+          title: 'Jeu 1',
+          description: 'Description du jeu 1',
+      },
+      {
+          image: 'https://picsum.photos/500/300/?image=10',
+          title: 'Jeu 2',
+          description: 'Description du jeu 2',
+      },
+      {
+          image: 'https://picsum.photos/500/300/?image=10',
+          title: 'Jeu 3',
+          description: 'Description du jeu 3',
+      },
+      {
+          image: 'https://picsum.photos/500/300/?image=10',
+          title: 'Jeu 4',
+          description: 'Description du jeu 4',
+      },
+      // Ajoutez les données pour les autres cartes ici
+  ];
   return (
-    <div className="Game-stats">
-      <h1>Game Stats</h1>
-    </div>
+        <div className="main">
+          <h1>Responsive Card Grid Layout</h1>
+          <ul className="cards">
+            {gameCardsData.map((card, index) => (
+              <li className="cards_item" key={index}>
+                <div className="card">
+                  <div className="card_image"><img src={card.image} alt={card.title} /></div>
+                  <div className="card_content">
+                    <h2 className="card_title">{card.title}</h2>
+                    <p className="card_text">{card.description}</p>
+                    <button className="btn card_btn">Read More</button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
   );
 }
-
-export default GameStats;
